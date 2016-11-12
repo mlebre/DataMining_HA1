3f1bc32639c53f854d88a70ba26828bb92421ec0
mlebre <marianne.lebre@insa-yon.fr>
2016-11-12 11:15:56 +0100

    Construction of the characteristic method in minHash class

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ class Shingling:
 		''' Construct shingles of length size (default value 5) 
 		    from the document name given
 		'''
-		self.k=size
+		self.k=size # shingle size
 		self.file=doc 
 		self.hashed_values=[]
+
 
 	def shingle(self):
 		global shingle_dictionary
@@ -19,35 +20,41 @@ class Shingling:
 		fi=open(self.file, 'r')
 		text=str()
 		for line in fi:
-			text=text+line
+			text=text+line # layout of the document taken in count
 		for i in xrange(len(text)):
 			if i+self.k>len(text):
 				break # avoid to create shingle under secified size
 			if text[i:i+self.k] not in shingle_dictionary.keys():
+				# Creates an entry in dictionnaries for new shingle
 				shingle_dictionary[text[i:i+self.k]]=len(shingle_dictionary)+1
 				hash_dictionary[shingle_dictionary[text[i:i+self.k]]]=text[i:i+self.k]
-			if shingle_dictionary[text[i:i+self.k]] not in self.hashed_values:
-				# Don't care about repetition of a shingle? 
-				self.hashed_values.append(shingle_dictionary[text[i:i+self.k]])
+			self.hashed_values.append(shingle_dictionary[text[i:i+self.k]]) # at the ith case of self.hashed_value there is the hash value associated to the ith shingle
 		fi.close()
 
+
 class minHashing:
-	def __init__(self, hash1, n):
-		self.set=hash1
-		self.M=self.characteristic_matrix()
+	def __init__(self, set1, n):
+		#assert  
+		self.set=set1 # set is under hashed value forms
 		self.nPermutations=n
+		self.M=list() # characteristic matrix
+		self.key=(hash_dictionary.keys()) # tuple to be sure we always work with the same list of keys (same order)
 
 	def characteristic_matrix(self):
-		''' Creates the characteristic matrix of the set associated with self
-			and only returns the positions where there is 1
-		'''
-		M=list()
-		for i in xrange(len(hash_dictionary)):
-			if hash_dictionary.keys()[i] in self.set:
-				M.append(i) # we are only interested in the position of where it's matches
-		return M
+		''' Creates the characteristic matrix of the set, only returns the positions where there is 1
+		''' 
+		for i in xrange(len(self.set)):
+			j=0
+			while self.set[i]!=self.key[j]:
+				j+=1
+				continue
+			self.M.append(self.key[j]) 
 
-	def minHash
+
+
+
+
+
 ###########################################################
 #                   Function definition
 ###########################################################
@@ -56,7 +63,6 @@ def compareSets(hash1, hash2):
 	''' Return the Jaccard similarity of 2 sets of of hash values
 	'''
 	inter=np.intersect1d(hash1, hash2)
-	print inter
 	union=set(hash1+hash2)
 	#print union
 	return len(inter)/float(len(union))
@@ -78,20 +84,23 @@ shigling_size=1
 doc1='a.txt'
 G=Shingling(doc1, shigling_size)
 G.shingle()
+
 #doc2='Data/Part1/awards_1990/awd_1990_00/a9000031.txt'
 doc2='b.txt'
 H=Shingling(doc2, shigling_size)
 H.shingle()
+
 doc3='c.txt'
 I=Shingling(doc3,shigling_size)
 I.shingle()
 print len(hash_dictionary)
-#print len(hash_dictionary[1])
-#print shingle_dictionary['Title    ']
 
 intersec=compareSets(G.hashed_values, H.hashed_values)
-print intersec
+print 'Jaccard similarity:', intersec
 
-print G.hashed_values
 minH=minHashing(I.hashed_values, 10)
+minH.characteristic_matrix()
+
 print minH.M
+for i in xrange(len(minH.M)):
+	print list(hash_dictionary[minH.M[i]])
